306323dfd61dca794ff69fba6e93877a20207d2f
xtina999 <avgustroder@gmail.com>
2024-04-29 15:49:55 +0300

    Added Character list endpoint with filter

--- characters/urls.py
+++ characters/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from characters.views import get_random_characters_view
+from characters.views import get_random_characters_view, CharacterListView
+
 app_name = 'characters'
 urlpatterns = [
     path("characters/random/", get_random_characters_view, name="character-random"),
+    path("characters/", CharacterListView.as_view(), name="character-list"),
+
 ]
--- characters/views.py
+++ characters/views.py
@@ -1,6 +1,7 @@
 import random
 
-from rest_framework import status, viewsets
+from django.db.models import QuerySet
+from rest_framework import status, viewsets, generics
 from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -16,3 +17,15 @@ def get_random_characters_view(request: Request) -> Response:
     random_character = Characters.objects.get(pk=random_pk)
     serializer = CharacterSerializer(random_character)
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class CharacterListView(generics.ListAPIView):
+    serializer_class = CharacterSerializer
+
+    def get_queryset(self) -> QuerySet:
+        queryset = Characters.objects.all()
+        name = self.request.query_params.get("name")
+        if name is not None:
+            queryset = queryset.filter(name__icontains=name)
+        return queryset
+
